d4fdb13bd9cd1dd278fbf947aa2ee254f5188691
paulacam <paula9cameron@gmail.com>
2019-02-17 06:57:15 -0700

    added a separate function file for horizon detection

---
+++ horizon.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import os
+
+def detect_horizon(pic):
+    # convert to grayscale
+    gray = cv.cvtColor(pic,cv.COLOR_BGR2GRAY)
+
+    # Canny Edge Detection
+    edges = cv.Canny(gray,125,175)
+
+    # Hough Line Transform
+    lines = cv.HoughLines(edges,1,np.pi/180,200)
+
+    xyList = []
+    imCopy = pic.copy()
+    for rho,theta in lines[0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        xyList = [(x1,y1), (x2,y2)]
+
+        cv.line(imCopy,(x1,y1),(x2,y2),(0,0,255),10)
+
+    x1 = xyList[0][0]
+    x2 = xyList[1][0]
+    y1 = xyList[0][1]
+    y2 = xyList[1][1]
+
+    # find slope and y-intercept
+    m = (y2-y1)/(x2-x1)
+    b = (x1/(x1-x2))*y2 - (x2/(x1-x2))*y1
+
+    horizon = pic.copy()
+    p1x = 0
+    p1y = int(b)
+    p2x = 3648
+    p2y = int(m*p2x + b)
+    cv.line(horizon,(p1x,p1y), (p2x,p2y), (255,0,0), 15)
+
+    return horizon
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 import os
+from horizon import detect_horizon
 
 show = 0 # if show = 1: displays figures, if show = 0: suppresses figures
 
@@ -56,51 +57,16 @@ for filename in os.listdir('../PrototypeCV/Inputs'):
         #plt.show()
 
         # find contours with binary image
-        imCopy = pic2.copy()
+        imCopy = pic.copy()
         contours, hierarchy = cv.findContours(threshB, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
         cv.drawContours(imCopy, contours, -1, (0,255,0), 3)
         #plt.imshow(imCopy)
         #plt.show()
 
-        # Canny Edge Detection
-        edges = cv.Canny(gray,125,175)
-        #plt.imshow(edges)
-        #plt.show()
-
-        # Hough Line Transform
-        lines = cv.HoughLines(edges,1,np.pi/180,200)
-
-        xyList = []
-        imCopy = pic2.copy()
-        for rho,theta in lines[0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
-            xyList = [(x1,y1), (x2,y2)]
-
-            cv.line(imCopy,(x1,y1),(x2,y2),(0,0,255),10)
-
-        x1 = xyList[0][0]
-        x2 = xyList[1][0]
-        y1 = xyList[0][1]
-        y2 = xyList[1][1]
-
-        m = (y2-y1)/(x2-x1)
-        b = (x1/(x1-x2))*y2 - (x2/(x1-x2))*y1
-
-        horizon = pic2.copy()
-        p1x = 0
-        p1y = int(b)
-        p2x = 3648
-        p2y = int(m*p2x + b)
-        cv.line(horizon,(p1x,p1y), (p2x,p2y), (255,0,0), 12)
-        #plt.imshow(horizon)
-        #plt.show()
+        # find horizon
+        horizon = detect_horizon(pic)
+        plt.imshow(horizon)
+        plt.show()
 
         # MSER
         _delta = 5
