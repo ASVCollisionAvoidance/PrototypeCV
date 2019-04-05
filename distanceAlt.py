Fixed sign and order of returned distances

distanceAlt.py
@@ -35,19 +35,16 @@ def findDistances(horizon, regionCount, points):
     for blob in transformed_points:
         minx.append(np.amin(blob[1]))
         maxx.append(np.amax(blob[1]))
-        miny.append(np.amin(blob[0]))
-        maxy.append(np.amax(blob[0]))
+        miny.append(np.amax(blob[0]))
+        maxy.append(np.amin(blob[0]))
         
     xref, yref = transformed_edge[0]-transformed_tip[0], transformed_edge[1]-transformed_tip[1]
-#    minxdist = (minx[minx[:]-p.tip[0]])/xref*p.xscale
-#    maxxdist = (maxx-p.tip[0])/xref*p.xscale
-#    minydist = (miny-p.tip[1])/yref*p.yscale
-#    maxydist = (maxy-p.tip[1])/yref*p.yscale
-    minxdist = list(map((lambda x: (x-p.tip[0])/xref*p.xscale), minx))
-    maxxdist = list(map((lambda x: (x-p.tip[0])/xref*p.xscale), maxx))
-    minydist = list(map((lambda x: (x-p.tip[1])/yref*p.yscale), miny))
-    maxydist = list(map((lambda x: (x-p.tip[1])/yref*p.yscale), maxy))
-
+    
+    minxdist = list(map((lambda x: (x-transformed_tip[0])/xref*p.xscale), minx))
+    maxxdist = list(map((lambda x: (x-transformed_tip[0])/xref*p.xscale), maxx))
+    minydist = list(map((lambda x: (transformed_tip[1]-x)/yref*p.yscale), miny))
+    maxydist = list(map((lambda x: (transformed_tip[1]-x)/yref*p.yscale), maxy))
+#    return miny, xref, yref, transformed_edge, transformed_tip, minydist
     dist = zip(minxdist, maxxdist, minydist, maxydist)
     
     return dist